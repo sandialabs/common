8767f51ab1223557cd75cff0dee59fcac66952e0
Alfie Johnson <aejohns@sandia.gov>
2024-09-19 13:49:00 -0600

    Just tweaked the header image

--- src/COMMONConfig/Properties/AssemblyInfo.cs
+++ src/COMMONConfig/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.5.3.107")]
-[assembly: AssemblyFileVersion("1.5.3.107")]
+[assembly: AssemblyVersion("1.5.3.108")]
+[assembly: AssemblyFileVersion("1.5.3.108")]
